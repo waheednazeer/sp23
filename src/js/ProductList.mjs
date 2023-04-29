5b61d1f028c2cdd1d94a7a1631ca1005b60abbb5
waheednazeer <waheed.nazeer@hotmail.com>
2023-04-29 14:43:40 +0500

    product filtered

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,44 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product) {
+  return `<li class="product-card">
+  <a href="product_pages/index.html?product=${product.Id}">
+  <img
+    src="${product.Image}"
+    alt="Image of ${product.Name}"
+  />
+  <h3 class="card__brand">${product.Brand.Name}</h3>
+  <h2 class="card__name">${product.Name}</h2>
+  <p class="product-card__price">$${product.FinalPrice}</p></a>
+</li>`;
+}
+
+function filterProducts(list) {
+  return list.slice(0, 4);
+}
+
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    // We passed in this information to make our class as reusable as possible.
+    // Being able to define these things when we use the class will make it very flexible
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+  async init() {
+    // our dataSource will return a Promise...so we can use await to resolve it.
+    const mylist = await this.dataSource.getData();
+    // render the list
+    this.renderList(mylist);
+  }
+  // render after doing the first stretch
+  renderList(mylist) {
+    renderListWithTemplate(productCardTemplate, this.listElement, filterProducts(mylist));
+  }
+
+  // render before doing the stretch
+  // renderList(list) {
+  //   const htmlStrings = list.map(productCardTemplate);
+  //   this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+  // }
+}
