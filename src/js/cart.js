acb655822df700ff9eb4dcf7d3b6e1671bd7bee3
waheednazeer <waheed.nazeer@hotmail.com>
2023-05-05 02:50:44 +0500

    dynamic header footer

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,7 @@
 import { getLocalStorage } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -38,6 +38,31 @@ export function renderListWithTemplate(
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
+// header footer
+// function to take an optional object and a template and insert the objects as HTML into the DOM
+//export function renderWithTemplate(template, parentElement, data, callback) {
+  export function renderWithTemplate(template, parentElement) {
+  parentElement.insertAdjacentHTML("afterbegin", template);
+ 
+}
+
+async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+// function to dynamically load the header and footer into a page
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
+
 
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
